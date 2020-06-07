b5d658645e8703d2615312d486099d5bfbc0fc7f
cho881020 <kj_cho@idealidea.co.kr>
2020-06-07 12:42:07 +0900

    http이미지도 불러올 수 있도록 변경 - 테스트

--- app/src/main/java/kr/co/tjoeun/library_20200607/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200607/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        Glide.with(mContext).load("https://m.wingbling.co.kr/web/upload/NNEditor/20191210/%EC%95%84%EC%8A%A4%ED%8A%B8%EB%A1%9C%20%EA%B3%B5%EC%8B%9D%20%ED%8A%B8%EC%9C%84%ED%84%B0%20-%20%EC%95%84%EC%8A%A4%ED%8A%B8%EB%A1%9C%20%EC%B0%A8%EC%9D%80%EC%9A%B0%20(%EC%85%80%EB%A0%88%EB%82%98%20%EB%AA%A9%EA%B1%B8%EC%9D%B4)_shop1_002508.jpg").into(profileImg)
+        Glide.with(mContext).load("http://file2.nocutnews.co.kr/newsroom/image/2018/07/18/20180718120948825781_0_420_600.jpg").into(profileImg)
 
     }
 
